94d2d513d96390fb2be6359f56883a86e0f3245c
mrcaidev <mrcaidev@gmail.com>
2022-08-08 15:11:30 +0800

    feat: useLatest

---
+++ src/use-latest.ts
@@ -0,0 +1,12 @@
+import { useRef } from "react";
+
+/**
+ * Cache a mutable value, and always return its latest value.
+ * @param value Initial value, expected to change later on.
+ * @returns A ref object containing the latest value.
+ */
+export function useLatest<T>(value: T) {
+  const ref = useRef<T>(value);
+  ref.current = value;
+  return ref;
+}
---
+++ tests/use-latest.test.ts
@@ -0,0 +1,22 @@
+import { renderHook } from "@testing-library/react";
+import { useLatest } from "../src/use-latest";
+
+describe("useLatest", () => {
+  it("sets up correctly", () => {
+    const { result } = renderHook(() => useLatest("hello"));
+    expect(result.current.current).toEqual("hello");
+  });
+
+  it("always returns latest value", () => {
+    const { result, rerender } = renderHook((count) => useLatest(count), {
+      initialProps: 0,
+    });
+    expect(result.current.current).toEqual(0);
+
+    rerender(1);
+    expect(result.current.current).toEqual(1);
+
+    rerender(2);
+    expect(result.current.current).toEqual(2);
+  });
+});
