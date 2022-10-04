c46ae5e8fb6cded88a0dfc4a0832cb923900587f
mrcaidev <mrcaidev@gmail.com>
2022-10-04 21:59:41 +0800

    refactor(config): use config from @mrcaidev

--- docs/astro.config.ts
+++ docs/astro.config.ts
@@ -1,6 +1,3 @@
-import image from "@astrojs/image";
-import mdx from "@astrojs/mdx";
-import prefetch from "@astrojs/prefetch";
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
@@ -9,5 +6,5 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   site: "https://hooks.mrcai.space",
-  integrations: [react(), image(), mdx(), prefetch(), sitemap(), tailwind()],
+  integrations: [react(), sitemap(), tailwind()],
 });
