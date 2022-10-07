4face8dd14a7372b213a9314023ee92cb756a677
mrcaidev <mrcaidev@gmail.com>
2022-10-07 14:29:19 +0800

    test: useDocument

---
+++ packages/hooks/tests/use-document.test.ts
@@ -0,0 +1,9 @@
+import { renderHook } from "@testing-library/react";
+import { useDocument } from "../src/use-document";
+
+describe("useDocument", () => {
+  it("can correctly set up and tear down", () => {
+    const { result } = renderHook(() => useDocument());
+    expect(result.current.current).toEqual(document);
+  });
+});
