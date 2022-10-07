98e41b8243757febe7763485bcf43a15906ac7b6
mrcaidev <mrcaidev@gmail.com>
2022-10-07 14:34:15 +0800

    test: useUnsafeOnceEffect

---
+++ packages/hooks/tests/use-unsafe-once-effect.test.ts
@@ -0,0 +1,26 @@
+import { renderHook } from "@testing-library/react";
+import { useUnsafeOnceEffect } from "../src/use-unsafe-once-effect";
+
+describe("useUnsafeOnceEffect", () => {
+  it("only runs once", () => {
+    const cleanup = jest.fn();
+    const effect = jest.fn();
+
+    const { rerender, unmount } = renderHook(() =>
+      useUnsafeOnceEffect(() => {
+        effect();
+        return cleanup;
+      })
+    );
+    expect(effect).toHaveBeenCalledTimes(1);
+    expect(cleanup).toHaveBeenCalledTimes(0);
+
+    rerender();
+    expect(effect).toHaveBeenCalledTimes(1);
+    expect(cleanup).toHaveBeenCalledTimes(0);
+
+    unmount();
+    expect(effect).toHaveBeenCalledTimes(1);
+    expect(cleanup).toHaveBeenCalledTimes(1);
+  });
+});
