4b4d39bfad4b540948f2382e398d5b7789f3e292
mrcaidev <mrcaidev@gmail.com>
2023-01-27 15:11:58 +0800

    fix: remove deprecated event targets

--- packages/hooks/src/use-event-listener.ts
+++ packages/hooks/src/use-event-listener.ts
@@ -1,49 +1,10 @@
 import { useEffect, type DependencyList, type RefObject } from "react";
 import { useLatest } from "./use-latest";
 
-export type UseEventListenerOptions = AddEventListenerOptions & {
-  extraDeps?: DependencyList;
-};
-
-/**
- * Listen for any events.
- */
-export function useEventListener<T extends EventTarget, K extends EventType<T>>(
-  ref: RefObject<T>,
-  type: K,
-  callback: (e: EventMap<T>[K]) => void,
-  options: UseEventListenerOptions = {}
-) {
-  const {
-    capture = false,
-    once = false,
-    passive = false,
-    extraDeps = [],
-  } = options;
-
-  const callbackRef = useLatest(callback);
-
-  useEffect(() => {
-    const target = ref.current;
-    if (!target) {
-      return;
-    }
-
-    const listener = callbackRef.current as EventListener;
-
-    target.addEventListener(type, listener, { capture, once, passive });
-    return () => target.removeEventListener(type, listener);
-
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [ref, type, callbackRef, capture, once, passive, ...extraDeps]);
-}
-
 export type EventMap<T extends EventTarget> = T extends HTMLVideoElement
   ? HTMLVideoElementEventMap
   : T extends HTMLBodyElement
   ? HTMLBodyElementEventMap
-  : T extends HTMLFrameSetElement
-  ? HTMLFrameSetElementEventMap
   : T extends HTMLMediaElement
   ? HTMLMediaElementEventMap
   : T extends SVGSVGElement
@@ -118,8 +79,6 @@ export type EventMap<T extends EventTarget> = T extends HTMLVideoElement
   ? MessagePortEventMap
   : T extends Notification
   ? NotificationEventMap
-  : T extends OffscreenCanvas
-  ? OffscreenCanvasEventMap
   : T extends PaymentRequest
   ? PaymentRequestEventMap
   : T extends Performance
@@ -144,8 +103,6 @@ export type EventMap<T extends EventTarget> = T extends HTMLVideoElement
   ? RemotePlaybackEventMap
   : T extends ScreenOrientation
   ? ScreenOrientationEventMap
-  : T extends ScriptProcessorNode
-  ? ScriptProcessorNodeEventMap
   : T extends ServiceWorkerContainer
   ? ServiceWorkerContainerEventMap
   : T extends ServiceWorkerRegistration
@@ -180,3 +137,40 @@ export type EventType<T extends EventTarget> = Extract<
   keyof EventMap<T>,
   string
 >;
+
+export type UseEventListenerOptions = AddEventListenerOptions & {
+  extraDeps?: DependencyList;
+};
+
+/**
+ * Listen for any events.
+ */
+export function useEventListener<T extends EventTarget, K extends EventType<T>>(
+  ref: RefObject<T>,
+  type: K,
+  callback: (e: EventMap<T>[K]) => void,
+  options: UseEventListenerOptions = {}
+) {
+  const {
+    capture = false,
+    once = false,
+    passive = false,
+    extraDeps = [],
+  } = options;
+
+  const callbackRef = useLatest(callback);
+
+  useEffect(() => {
+    const target = ref.current;
+    if (!target) {
+      return;
+    }
+
+    const listener = callbackRef.current as EventListener;
+
+    target.addEventListener(type, listener, { capture, once, passive });
+    return () => target.removeEventListener(type, listener);
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [ref, type, callbackRef, capture, once, passive, ...extraDeps]);
+}
