552b5fbe0c10c82372ec272f451e49618ce7d8ed
Yuwang Cai <mrcaidev@gmail.com>
2024-11-13 14:18:45 +0800

    refactor: use-mount

--- src/use-mount.ts
+++ src/use-mount.ts
@@ -1,7 +1,7 @@
 import { useEffect, type EffectCallback } from "react";
 
 /**
- * Trigger effect after the component is mounted.
+ * Trigger an effect only after the component is mounted.
  */
 export function useMount(effect: EffectCallback) {
   // eslint-disable-next-line react-hooks/exhaustive-deps
--- tests/use-mount.test.ts
+++ tests/use-mount.test.ts
@@ -1,19 +1,27 @@
 import { renderHook } from "@testing-library/react";
 import { useMount } from "src";
 
-it("only runs on mount", () => {
+it("runs only on mount", () => {
   const cleanup = vi.fn();
   const effect = vi.fn(() => cleanup);
 
   const { rerender, unmount } = renderHook(() => useMount(effect));
+
   expect(effect).toHaveBeenCalledTimes(1);
   expect(cleanup).toHaveBeenCalledTimes(0);
 
   rerender();
+
+  expect(effect).toHaveBeenCalledTimes(1);
+  expect(cleanup).toHaveBeenCalledTimes(0);
+
+  rerender();
+
   expect(effect).toHaveBeenCalledTimes(1);
   expect(cleanup).toHaveBeenCalledTimes(0);
 
   unmount();
+
   expect(effect).toHaveBeenCalledTimes(1);
   expect(cleanup).toHaveBeenCalledTimes(1);
 });
