45909448a5374ee4b2e2468ec348ed9d28987fb9
Yuwang Cai <mrcaidev@gmail.com>
2024-11-13 11:52:05 +0800

    refactor: use-latest

--- tests/use-latest.test.ts
+++ tests/use-latest.test.ts
@@ -5,12 +5,15 @@ it("returns latest primitive", () => {
   const { result, rerender } = renderHook((count) => useLatest(count), {
     initialProps: 0,
   });
+
   expect(result.current.current).toEqual(0);
 
   rerender(1);
+
   expect(result.current.current).toEqual(1);
 
   rerender(2);
+
   expect(result.current.current).toEqual(2);
 });
 
@@ -18,12 +21,15 @@ it("returns latest object", () => {
   const { result, rerender } = renderHook((obj) => useLatest(obj), {
     initialProps: { a: 1 },
   });
+
   expect(result.current.current).toEqual({ a: 1 });
 
   rerender({ a: 2 });
+
   expect(result.current.current).toEqual({ a: 2 });
 
   rerender({ a: 3 });
+
   expect(result.current.current).toEqual({ a: 3 });
 });
 
@@ -31,11 +37,14 @@ it("returns latest function", () => {
   const { result, rerender } = renderHook((fn) => useLatest(fn), {
     initialProps: () => 1 as number,
   });
+
   expect(result.current.current()).toEqual(1);
 
   rerender(() => 2);
+
   expect(result.current.current()).toEqual(2);
 
   rerender(() => 3);
+
   expect(result.current.current()).toEqual(3);
 });
