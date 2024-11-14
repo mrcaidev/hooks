e75f850a34fc769a7abe91496a67b15225108913
Yuwang Cai <mrcaidev@gmail.com>
2024-11-14 16:16:57 +0800

    docs: use-const-fn

--- src/use-const.ts
+++ src/use-const.ts
@@ -1,7 +1,7 @@
 import { useMemo } from "react";
 
 /**
- * Use an immutable value, which stays the same across re-renders.
+ * Memoize a value, which stays the same across rerenders.
  */
 export function useConst<T>(factory: () => T) {
   return useMemo(factory, []);
